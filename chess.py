d0e63330a9f39c7175eb0062a4e3aa8181544a3c
Juho Timonen <juho.timonen@aalto.fi>
2021-09-24 23:12:19 +0300

    separate UI things to ui.py

--- chess.py
+++ chess.py
@@ -1,4 +1,3 @@
-from ai import AIGame
 import math
 import numpy
 import sys
@@ -7,78 +6,41 @@ import pygame
 from pygame.locals import KEYDOWN, MOUSEBUTTONDOWN, QUIT, K_b, K_c, K_v
 import concurrent.futures
 
+from ai import AIGame
 from board import Board, Move, Position
+from ui import create_background, create_piece_sprites, create_square_sprites
 
+# Initialize and create UI
 executor = concurrent.futures.ThreadPoolExecutor(max_workers = 1)
-
 pygame.init()
 screen = pygame.display.set_mode((480, 520))
 pygame.display.set_caption("Chess")
+background = create_background(screen)
 
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill((120, 120, 120))
-colors = [(80, 80, 80), (160, 160, 160)]
-for x in range(8):
-    for y in range(8):
-        color = colors[(x + y) % 2]
-        base_x = x * 60 + 1
-        base_y = (7 - y) * 60 + 1
-        rect = (base_x, base_y, 58, 58)
-        background.fill(color, rect)
+# Create sprites and fonts
+piece_sprites = create_piece_sprites(40)
+hover_sprite, selected_sprite, ai_sprite = create_square_sprites(58)
+text_color = (250, 250, 250)
+bottom_font = pygame.font.Font(None, 36)
+computing_text = bottom_font.render("Computing next move...", 1, text_color)
+done_text = bottom_font.render("Done!", 1, text_color)
 
-font = pygame.font.Font(None, 36)
-PIECE_SIZE = 40
-
-
-def piece_sprite(col, typ, SIZE: int):
-    im = pygame.image.load("sprites/" + col + "_" + typ + "_png_128px.png")
-    return pygame.transform.scale(im.convert_alpha(), (SIZE, SIZE))
-
-
-piece_sprites = {
-    1 : {
-        1 : piece_sprite("w", "pawn", PIECE_SIZE),
-        2 : piece_sprite("w", "knight", PIECE_SIZE),
-        3 : piece_sprite("w", "bishop", PIECE_SIZE),
-        4 : piece_sprite("w", "rook", PIECE_SIZE),
-        5 : piece_sprite("w", "queen", PIECE_SIZE),
-        6 : piece_sprite("w", "king", PIECE_SIZE),
-    },
-    -1 : {
-        1 : piece_sprite("b", "pawn", PIECE_SIZE),
-        2 : piece_sprite("b", "knight", PIECE_SIZE),
-        3 : piece_sprite("b", "bishop", PIECE_SIZE),
-        4 : piece_sprite("b", "rook", PIECE_SIZE),
-        5 : piece_sprite("b", "queen", PIECE_SIZE),
-        6 : piece_sprite("b", "king", PIECE_SIZE),
-    }
-}
-
-computing_text = font.render("Computing next move...", 1, (250, 250, 250))
-done_text = font.render("Done!", 1, (250, 250, 250))
-hover_sprite = pygame.Surface((58, 58))
-hover_sprite.fill((200, 120, 120))
-selected_sprite = pygame.Surface((58, 58))
-selected_sprite.fill((220, 220, 120))
-ai_sprite = pygame.Surface((58, 58))
-ai_sprite.fill((120, 120, 220))
-
+# Create clock and board
 clock = pygame.time.Clock()
-
 board = Board()
 
-def do_ai(board : Board, active_player_sign : int):
-    ai = AIGame(board)
-    move = ai.pick_next_move(active_player_sign)
-    return (move.src, move.dst)
-
+# Global state
 selected = None
 hovered = None
 moves = []
 ai = None
 ai_src = None
 ai_dst = None
+
+def do_ai(board : Board, active_player_sign : int):
+    ai = AIGame(board)
+    move = ai.pick_next_move(active_player_sign)
+    return (move.src, move.dst)
 
 def draw():
     global selected
@@ -87,17 +49,18 @@ def draw():
     global ai
     global ai_src
     global ai_dst
-    screen_x, screen_y = pygame.mouse.get_pos()
 
+    # Determine what square the cursor is on
+    screen_x, screen_y = pygame.mouse.get_pos()
     x = math.floor(screen_x / 60)
     y = math.floor((480 - screen_y) / 60)
     pos = Position(x, y)
-
     if x < 8 and y < 8:
         hovered = pos
     else:
         hovered = None
 
+    # Event handling, perform moves
     for event in pygame.event.get():
         if event.type == QUIT:
             sys.exit()
@@ -129,7 +92,8 @@ def draw():
     if ai is not None and ai.done():
         ai_src, ai_dst = ai.result()
         ai = None
-    
+
+    # Draw sprites
     screen.blit(background, (0, 0))
     for x in range(8):
         for y in range(8):
@@ -172,7 +136,7 @@ def draw():
 
     pygame.display.flip()
 
+# Game loop
 while 1:
     draw()
     clock.tick(60)
-
